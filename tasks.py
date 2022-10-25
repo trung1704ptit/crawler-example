7ecf2e4e2bb998465418465717cd908a5ec0805d
Trung Nguyen <trung1704ptit@gmail.com>
2022-10-25 10:03:11 +0700

    update redis connection

--- tasks.py
+++ tasks.py
@@ -2,16 +2,38 @@ from celery import Celery
 import requests 
 from bs4 import BeautifulSoup 
 from urllib.parse import urljoin
+from redis import Redis
+
+connection = Redis(db=1)
  
 app = Celery('tasks', broker_url='redis://default:redis@127.0.0.1:6379/1') 
  
 @app.task 
-def crawl(url): 
+def crawl(url):
+	connection.sadd('crawling:queued', url) # Add url to set
 	html = get_html(url) 
 	soup = BeautifulSoup(html, 'html.parser') 
 	links = extract_links(url, soup) 
-	print(links) 
- 
+	for link in links:
+		if allow_url_filter(link) and not seen(link):
+			print('Add URL to visit queue', link)
+			add_to_visit(link)
+	
+	# automically move a URL from queued to visited
+	connection.smove('crawling:queued', 'crawling:visited', url)
+
+def allow_url_filter(url):
+	return '/shop/page/' in url and '#' not in url
+
+def seen(url):
+	return connection.sismember('crawling:visited', url) or connection.sismember('crawling:queued', url)
+
+
+def add_to_visit(url):
+	# if LPOS is not available in Redis library
+	if connection.execute_command('LPOS', 'crawling:to_visit', url) is None:
+		connection.rpush('crawling:to_visit', url) ## add URL to the end of this list
+
 def get_html(url): 
 	try: 
 		response = requests.get(url) 
@@ -27,6 +49,3 @@ def extract_links(url, soup):
 		for a in soup.find_all('a') 
 		if a.get('href') and not(a.get('rel') and 'nofollow' in a.get('rel')) 
 	}) 
- 
-starting_url = 'https://scrapeme.live/shop/page/1/' 
-crawl(starting_url)
